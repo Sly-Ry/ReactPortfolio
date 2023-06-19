54b693d297b98afeab940a286e94852bacddd380
Ryan Williams <ryan.williams93@aol.com>
2023-06-19 01:26:35 -0500

    added full name to homepage

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,9 +3,14 @@ import '../styles/Home.css'
 export function Home() {
     return (
         <div className="home_page">
+            
             <h1>This is</h1>
             <h2>RAW</h2>
-            <p>Talent</p>
+            <div className="name_box">
+                <p className='talent'>Ryan</p>
+                <p className='talent'>Alexander</p>
+                <p className='talent'>Williams</p>
+            </div>
         </div>
     ) 
 }
