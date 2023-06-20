a66608d8ff50956e0c5c09f0a6fc4d1e5aad6592
Ryan Williams <ryan.williams93@aol.com>
2023-06-20 16:23:55 -0500

    added form submisison directly to personal email

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -15,32 +15,38 @@ export function Contact() {
     return (
         <>
         <div className="Contact d-flex justify-content-center">
-            <div className="container">
-                    <div className="cont_box row">
+            <div className="container" >
+                    <form className="cont_box row" action="https://formsubmit.co/0b597d519a913e26543ffbfe54a6fa12" method="POST">
+                        {/* Spammer Catch */}
+                        <input type='text' name="_honey" style={{display: 'none'}}/>
+
+                        {/* Disable Captcha */}
+                        <input type='hidden' name="_captcha" value={'false'}/>
+
                         <div className="c_box col-12 col-md-7">
                             <div className="mb-3">
                                 <label htmlFor="name" className="form-label">
                                     Name
                                 </label>
-                                <input type="text" className="form-control" id="name" placeholder="Enter your name"/>
+                                <input type="text" className="form-control" name="Name" id="name" placeholder="Enter your name"/>
                             </div>
                             <div className="mb-3">
                                 <label htmlFor="email" className="form-label">
                                     Email
                                 </label>
-                                <input type="email" className="form-control" id="email" placeholder="Enter your email"/>
+                                <input type="email" className="form-control" name="Email" id="email" placeholder="Enter your email"/>
                             </div>
                             <div className="mb-3">
                                 <label htmlFor="number" className="form-label">
                                     Contact Number
                                 </label>
-                                <input type="number" className="form-control" id="number" placeholder="#" onWheel={disableScrolling}/>
+                                <input type="number" className="form-control" name="Number" id="number" placeholder="#" onWheel={disableScrolling}/>
                             </div>
                             <div className="mb-3">
                                 <label htmlFor="message" className="form-label">
                                     Message
                                 </label>
-                                <textarea className="form-control" id="message" rows="7"></textarea>
+                                <textarea className="form-control" name='Message' id="message" rows="7"></textarea>
                             </div>
                             <button type='submit' className='btn btn-primary'>Send</button> 
                         </div>
@@ -90,7 +96,7 @@ export function Contact() {
                                 <hr/>
                             </div>
                         </div>
-                    </div>
+                    </form>
                 </div>
             </div>
         </>
